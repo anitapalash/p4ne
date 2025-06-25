97740139d9a004c8b618c0640c7115a7c7ce5f4f
Курдина Анна Ивановна <ai.kurdina@jet.su>
2025-06-25 16:06:41 +0300

    lab work 2.1 refactoring

--- Lab2.1/netdevice.py
+++ Lab2.1/netdevice.py
@@ -1,13 +1,17 @@
-import paramiko, time, requests
-from pprint import pprint
+import paramiko, time, requests, re
 
+PASSWORD = 'j0sg1280-7@'
+USERNAME = 'restapi'
+HOSTNAME = '10.31.70.209'
 BUF_SIZE = 50000
 TIMEOUT = 1
+
+print("Interfaces by paramiko")
 
 ssh_connection = paramiko.SSHClient()
 ssh_connection.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
-ssh_connection.connect(hostname='10.31.70.209', username='restapi', password='j0sg1280-7@')
+ssh_connection.connect(hostname=HOSTNAME, username=USERNAME, password=PASSWORD)
 session = ssh_connection.invoke_shell()
 
 session.send("\n".encode())
@@ -16,15 +20,18 @@ session.send("\nterminal length 0\n".encode())
 time.sleep(2)
 session.send("\nshow interface\n".encode())
 time.sleep(2)
-buf = session.recv(BUF_SIZE)
-print(buf.decode())
+result = session.recv(BUF_SIZE).decode()
+
+for line in re.finditer("((Internet address is )([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))|(([0-9]+ packets )(input|output), [0-9]+ bytes)", result):
+    print(line.group(0))
 
 ssh_connection.close()
 
 headers = { 'accept': 'application/yang-data+json', 'Content-type': 'application/yang-data+json'}
 r = requests.get('https://10.31.70.209/restconf/data/Cisco-IOS-XE-interfaces-oper:interfaces', headers=headers,
-                 verify=False, auth=('restapi', 'j0sg1280-7@'))
-pprint(r.json())
+                 verify=False, auth=(USERNAME, PASSWORD))
+# pprint(r.json())
+print("Interfaces by REST")
 
 for interface in r.json()['Cisco-IOS-XE-interfaces-oper:interfaces']['interface']:
     ip4v = interface['ipv4']
